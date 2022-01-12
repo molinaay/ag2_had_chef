0ec1777eacaad4be08c25a043998c670c69ee209
juan pablo sainea <juan.sainea@MBP-de-juan.lan>
2022-01-12 17:13:55 -0500

    pruebas unitarias completas

--- Chef_Wordpress/cookbooks/wordpress/spec/unit/recipes/default_spec.rb
+++ Chef_Wordpress/cookbooks/wordpress/spec/unit/recipes/default_spec.rb
@@ -2,12 +2,25 @@ require 'spec_helper'
 
 describe 'wordpress::instalador_dependencias' do
     platform 'ubuntu'
-
-    context 'file and necessary package' do
-        it "necessary package" do
+#prueba para ver instalacion de php
+    context 'instalacion correcta de php' do
+        it "paquete necesario se instala para php" do
             expect(chef_run).to install_package("php") 
         end
-
+        
+    end
+#prueba para ver instalacion de mysql
+    context 'instalacion de mysql' do
+        it "paquete necesario se instala para mysql" do
+            expect(chef_run).to install_package("mysql-server")
+        end
+    end
+#prueba para ver instalacion de apache
+    context 'instalacion de apache' do
+        it "paquete necesario se instala para apache" do
+            expect(chef_run).to install_package("apache2")
+        end
+#prueba para ver que no haya errores en la instalacion de los paquetes restantes
         it 'ejecucion correcta' do
             expect { chef_run }.to_not raise_error
         end
@@ -16,4 +29,18 @@ describe 'wordpress::instalador_dependencias' do
 
 end
 
+describe 'wordpress::instalador_aplicacion' do
+    platform 'ubuntu'
+#prueba para ver que se descargara wordpress
+    it "wordpress directory is created" do
+            expect(chef_run).to create_remote_file("/srv/www/latest.tar.gz")
+    end
+end
 
+describe 'wordpress::configurador_apache' do
+    platform 'ubuntu'
+#prueba para ver que se copie la configuracion de wordpress para apache
+    it "fichero de config de apache con wordpress copiado" do
+            expect(chef_run).to create_template("/etc/apache2/sites-available/wordpress.conf")
+    end
+end
