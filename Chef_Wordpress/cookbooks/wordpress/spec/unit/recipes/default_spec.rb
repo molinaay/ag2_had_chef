6a570d6d0c03ce5b2193035e6573b65c6b9e5d2f
juan pablo sainea <juan.sainea@MBP-de-juan.lan>
2022-01-11 23:00:39 -0500

    limpiando comentarios innecesarios en test unitario

--- Chef_Wordpress/cookbooks/wordpress/spec/unit/recipes/default_spec.rb
+++ Chef_Wordpress/cookbooks/wordpress/spec/unit/recipes/default_spec.rb
@@ -14,15 +14,6 @@ describe 'wordpress::instalador_dependencias' do
 
     end
 
-    # context "with 'source' as install_method" do
-    #   
-    #   override_attributes['vim']['install_method'] = 'source' 
-
-    #   it "should include the vim_pruebas_chef::source recipe when install_method='source'" do 
-    #     expect(chef_run).to include_recipe('vim_pruebas_chef::source') 
-    #   end
-    # end
-
 end
 
 
